e6bb7018f0a2ea7433e48b5c5f6d4e45254d58da
Mike Magnani <mikemagnani5@gmail.com>
2016-09-09 11:45:44 -0400

    added function for counter

--- script.js
+++ script.js
@@ -5,4 +5,35 @@ var intervalHandle;
 function resetPage(){
     document.getElementById("inputArea").style.display = "block";
 }
+function tick () {
+  // grab h1
+  var timeDisplay = document.getElementsById('time');
+  // turn seconds into mm:ss
+  var min = Math.floor(secondsRemainig / 60);
+
+  var sec = secondsRemainig - (min * 60);
+
+  //add a leading zero (as a string) if seconds less than 10
+  if (sec < 10){
+    sec = "0" + sec;
+  }
+  //subtract from seconds remaining
+  secondsRemainig--;
+}
+
+function startCountdown() {
+  // get contents of minutes in text box
+  var minutes = document.getElementById('minutes').value;
+  // check if not a number
+  if (isNaN(minutes)) {
+    alert("Enter a Number");
+    return;
+  }
+  // how many seconds
+  secondsRemainig = minutes * 60;
+  //call the tick function
+  intervalHandle = setInterval(tick,1000);
+  // hide the form
+  document.getElementById('inputArea').style.display = "none";
+}
 
